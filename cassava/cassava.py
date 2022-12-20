4b05efbc63aec3db5d34204b22a1c122fd82e28b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-20 19:34:07 -0300

    New exercise in Python - another sum

---
+++ cassava/cassava.py
@@ -0,0 +1,8 @@
+curupira = 300
+boitata = 1500
+boto = 600
+mapinguari = 1000
+iara = 150
+donaChica = 225
+portion = int(curupira) + int(boitata) + int(boto) + int(mapinguari) + int(iara) + int(donaChica)
+print("The total weight of cassava to be purchased will be " + str(portion))
