04490baf9d68d562859c812738bc93540e7f17af
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-01 16:06:46 -0300

    New way to format date

--- date/date1.py
+++ date/date1.py
@@ -5,3 +5,6 @@ print(currentDate)
 
 dateInText = '{}/{}/{}'.format(currentDate.day, currentDate.month, currentDate.year)
 print(dateInText)
+
+newDateInText = currentDate.strftime('%d/%m/%Y')
+print(newDateInText)
