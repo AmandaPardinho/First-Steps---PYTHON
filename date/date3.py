4dc1d2122b81daa3caf7f1a65a0c456e37b7454e
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-02 20:49:26 -0300

    Calculation with dates

--- date/date3.py
+++ date/date3.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, time, datetime, timedelta
+# from datautil.relativedelta import relativedelta
 
 date1 = date.today()
 print(date1)
 
-date2 = date1 + 2
+date2 = date.today() + timedelta(days=8)
+print(date2)
+
