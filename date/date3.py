8bef6dc4e4803ea7d3f641794f8c06dbcf101cda
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-02 21:07:31 -0300

    Calculation using another python library to calculate dates

--- date/date3.py
+++ date/date3.py
@@ -1,5 +1,5 @@
-from datetime import date, time, datetime, timedelta
-# from datautil.relativedelta import relativedelta
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 
 date1 = date.today()
 print(date1)
@@ -7,3 +7,8 @@ print(date1)
 date2 = date.today() + timedelta(days=8)
 print(date2)
 
+date3 = date.today() + timedelta(days=-365)
+print(date3)
+
+date4 = date3 + relativedelta(years=+8, months=+5, days=+9)
+print(date4)
